567cc2ce38247e2f6413fae627070386fd50161e
mywang-berk <mywang@berkeley.edu>
2025-10-11 16:22:04 -0700

    non-working fake pytorch impl

---
+++ assignment1-basics/cs336_basics/fake_pytorch.py
@@ -0,0 +1,11 @@
+"""Pedagogical Implementation of Some Pytorch Modules for Use in Transformer"""
+
+from torch import nn
+
+class Linear(Module):
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        pass
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+
